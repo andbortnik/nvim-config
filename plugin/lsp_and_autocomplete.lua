a52f8cbde36472dfa5facef361ef102ceb770927
Andrey Bortnikov <and.bortnik@gmail.com>
2022-08-28 15:24:36 +0300

    configure LSP for C/C++

--- plugin/lsp_and_autocomplete.lua
+++ plugin/lsp_and_autocomplete.lua
@@ -79,3 +79,19 @@ lspconfig['rust_analyzer'].setup({
         }
     }
 })
+
+lspconfig['ccls'].setup {
+    init_options = {
+        compilationDatabaseDirectory = "build";
+        index = {
+          threads = 0;
+        };
+        clang = {
+          excludeArgs = { "-frounding-math"} ;
+        };
+    };
+    cache = {
+      directory = ".ccls-cache";
+    };
+}
+
