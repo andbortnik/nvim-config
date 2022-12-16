0ed3ce06a9bc6bec6e18f87bfbdefdcc208111f5
Andrey Bortnikov <and.bortnik@gmail.com>
2022-12-16 18:29:10 +0300

    Update deprecated function use

--- plugin/lsp_and_autocomplete.lua
+++ plugin/lsp_and_autocomplete.lua
@@ -57,7 +57,7 @@ cmp.setup({
 
 -- Setup language servers
 lspconfig.pyright.setup {
-    capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     flags = {
         debounce_text_changes = 150,
     }
