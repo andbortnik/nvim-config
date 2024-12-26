399575bb1336c12d7aaabd0ea2e27a2a3e8bc703
Andrey Bortnikov <and.bortnik@gmail.com>
2024-12-26 11:15:01 +0100

    change diagnostic signs

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -3,10 +3,10 @@ vim.diagnostic.config({
     underline = false,
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.INFO] = "",
-            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.ERROR] = "●",
+            [vim.diagnostic.severity.WARN] = "●",
+            [vim.diagnostic.severity.INFO] = "●",
+            [vim.diagnostic.severity.HINT] = "",
         },
         linehl = {
             -- [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
